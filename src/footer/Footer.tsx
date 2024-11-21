6fa1f749a7db0b3601d7bd3e525bd2bffa42bdc9
lokesh-chowdary <lokeshchowdary475@gmail.com>
2024-11-21 18:00:01 +0530

    footer Files

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Footer.css';
+const Footer =()=>{
+    return(
+        <div>
+            <h1 className="praveen">praveen</h1>
+        </div>
+    );
+};
+export default Footer
