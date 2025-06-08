b465b2f726e96a12d7e217d9b22cb4af58666990
Praveen <kollurupraveen126@.com>
2025-06-08 14:53:37 +0530

    added food filter in filters

--- src/components/home/SearchFilters.tsx
+++ src/components/home/SearchFilters.tsx
@@ -21,6 +21,7 @@ interface Amenities {
   gym: boolean;
   fridge: boolean;
   evCharging: boolean;
+  food: boolean;
 }
 
 interface FilterState {
@@ -49,6 +50,7 @@ const initialFilters: FilterState = {
     gym: false,
     fridge: false,
     evCharging: false,
+    food: false,
   },
 };
 
@@ -62,6 +64,7 @@ const amenitiesList = [
   { key: 'gym', label: 'Gym', icon: Dumbbell },
   { key: 'fridge', label: 'Fridge', icon: Tv },
   { key: 'evCharging', label: 'EV Charging', icon: BatteryCharging },
+  {key: 'food', label: 'Food', icon: Tv },
 ];
 
 const SearchFilters: React.FC<SearchFiltersProps> = ({ filters, setFilters }) => {
